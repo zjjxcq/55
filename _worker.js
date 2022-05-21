6ff64928c1bcdd350009aa3e83c83c0cb3da432f
甬哥kkkyg <90416692+kkkyg@users.noreply.github.com>
2022-05-21 10:29:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="va2a.herokuapp.com";
+        url.hostname="warp.herokuapp.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
